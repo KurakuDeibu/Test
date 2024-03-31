fd5f58251471fa68b940b85ae661ad056ee29070
KurakuDeibu <davixdelapena30@gmail.com>
2024-03-31 23:08:40 +0800

    Changes on python file

--- Test.py
+++ Test.py
@@ -1,3 +1,26 @@
-This is added using git commands and gitbash
+class Student:
 
---Added changes using VIM commands--
+
+    def __init__(self, name, age, grade):
+        self._name = name
+        self._age = age
+        self._grade = grade
+
+    def get_Info(self):
+        print("Name: ", _name)
+        print("Age: ", _age)
+        print("Grade: ", _grade)
+
+    def set_grade(self, grade):
+        if 0 <= grade <= 100:
+            self._grade = grade
+
+    student1 = Student("Clark", 18, 90)
+
+    
+    student1.get_Info()
+
+    student1.set_grade(80)
+
+    student1.get_Info()
+
